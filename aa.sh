aaeba3165f1f56ba7f92c9aabc1b82ca71527e63
root <root@localhost.localdomain>
2019-06-22 00:03:42 +0000

    comm

---
+++ aa.sh
@@ -0,0 +1,14 @@
+
+git add .
+git commit -m "comm"
+git push
+exit
+
+
+
+echo "# terrakota" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/lukdeboss/terrakota.git
+git push -u origin master
